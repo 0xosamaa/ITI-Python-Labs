67d0690fa2b90c5f20681343eac72547f87fe216
Osama <0xm.osamaa@gmail.com>
2023-04-08 01:54:48 +0200

    task02

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ def vowels_count(text):
     return count
 
 print(vowels_count("The quick brown for jumps over the lazy dog."))
+
+def numbers_list(length, start):
+    numbers = []
+    for i in range(start, start + length):
+        numbers.append(i)
+    return numbers
+
+
+print(numbers_list(10, 5))
