41197ac4993bcf153df494d4c8c72385f68d435f
Osama <0xm.osamaa@gmail.com>
2023-04-08 02:34:54 +0200

    task05

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ def vowels_count(text):
             count += 1
     return count
 
+
 print(vowels_count("The quick brown for jumps over the lazy dog."))
+
 
 def numbers_list(length, start):
     numbers = []
@@ -28,17 +30,32 @@ def reverse_list(inputs):
 print(reverse_list())
 
 def fizz_buzz(num):
-    if not(num % 5):
-        if not(num % 3):
+    if not (num % 5):
+        if not (num % 3):
             return "FizzBuzz"
         return "Buzz"
-    
-    if not(num % 3):
-        if not(num % 5):
+
+    if not (num % 3):
+        if not (num % 5):
             return "FizzBuzz"
         return "Fizz"
-    
+
     return None
+
 
 print(fizz_buzz(15))
 
+text = input("Enter text:\n")
+
+
+def reverse_string(text):
+    char_list = list(text)
+    reversed_char_list = []
+    for char in char_list:
+        reversed_char_list.insert(0, char)
+    reversed_string = "".join(reversed_char_list)
+
+    return reversed_string
+
+
+print(reverse_string(text))
